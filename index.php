7a8df6756658b6be9ab0738dd02dc5c1ec3ba74b
emanuelefarelli <emaaliens888@gmail.com>
2023-07-13 17:13:34 +0200

    Added two methods and printed data on html

--- index.php
+++ index.php
@@ -7,13 +7,39 @@ class Movie{
     public $poster;
     public $genre;
 
-    function__construct($_title,$_timing,$_poster,$_genre){
+    function __construct($_title,$_timing,$_poster,$_genre){
         $this->title = $_title;
         $this->timing = $_timing;
         $this->poster = $_poster;
         $this->genre = $_genre;
+
+    }
+
+    public function getTitle(){
+        return $this->title;
+    }
+    public function getAll(){
+        return ($this->title.' - '.
+                $this->timing.' - '.
+                $this->poster.' - '.
+                $this->genre);
+
     }
 
 }
 
+$Interstellar = new Movie('Interstellar','2.50 h','link.img','fantascienza');
 ?>
+
+
+<h1>
+    <?php
+    echo $Interstellar->getTitle();
+    ?>
+</h1>
+
+<p>
+    <?php
+    echo $Interstellar->getAll();
+    ?>
+</p>
