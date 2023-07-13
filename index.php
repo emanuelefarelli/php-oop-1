439514d72d891fb4942f38b115e3b94fe7144d2a
emanuelefarelli <emaaliens888@gmail.com>
2023-07-13 16:48:20 +0200

    Added constructor to class Movie

--- index.php
+++ index.php
@@ -7,6 +7,12 @@ class Movie{
     public $poster;
     public $genre;
 
+    function__construct($_title,$_timing,$_poster,$_genre){
+        $this->title = $_title;
+        $this->timing = $_timing;
+        $this->poster = $_poster;
+        $this->genre = $_genre;
+    }
 
 }
 
